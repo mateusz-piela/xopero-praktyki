5a2b8399d04f2847378832132134378f8af23d37
mateusz-piela <mateusz.piela@zsegw.pl>
2023-04-21 14:35:52 +0200

    fix

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string a = "Hello world";
+            Console.WriteLine(a);
         }
     }
 }
