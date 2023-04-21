9a34e8af56b2fc22af658efa938ca827dba213c1
mateusz-piela <mateusz.piela@zsegw.pl>
2023-04-21 14:40:15 +0200

    new fix

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,6 +6,9 @@
         {
             string a = "Hello world";
             Console.WriteLine(a);
+            int num = 60;
+            int add = 9;
+            Console.WriteLine(add+num);
         }
     }
 }
